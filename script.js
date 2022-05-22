cb53417d60498dcc015db95f24ee012d90c7cdbf
mrc-sprinklr <mayukh.raychaudhuri@sprinklr.com>
2022-05-22 13:47:20 +0530

    move ellipsis logic from css to js :)

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-// html blocks to inject at runtime
-const generateTitleHtml = (title) => {
-  if (title.length <= 12)
-    return `<strong><span class="text-end">${title}</span></strong>`;
-  else
-    return `<strong><span class="text-ellipsis">${title.slice(0, -8)}
-    </span><span class="text-end">${title.slice(-8)}</span></strong>`;
-};
-
-const generateImageHtml = (src, title) => {
+// html-blocks to inject at runtime
+const generateImageHtml = (src, title, index) => {
   return `<div class="flex-item">
                 <div class="grid-item2-img"><img class="img2"
                          src=${src}
                          alt="load error"></div>
-                <div class="grid-item2">${generateTitleHtml(title)}</div>
+                <div class="grid-item2-text" id="div-${index}">${title.trim()}</div>
             </div>`;
+};
+
+const titleMap = new Map(); // to store the original title
+const contractTitle = (div) => {
+  const title = div.innerHTML;
+  titleMap.set(div.id, title);
+
+  // both considers only-padding, when border is zero
+  const excessWidth = div.scrollWidth - div.offsetWidth;
+  if (excessWidth > 0) {
+    const len = Math.floor((title.length - excessWidth / 20) / 2);
+    let [title1, title2, flag] = [
+      title.slice(0, len),
+      title.slice(-len + 1),
+      true,
+    ];
+    while (div.scrollWidth > div.offsetWidth) {
+      if (flag) title1 = title1.slice(0, -1);
+      else title2 = title2.slice(1);
+
+      div.innerHTML = `${title1}...${title2}`;
+      flag = !flag;
+    }
+  }
 };
 
 // load data from json
 const sidePanel = document.querySelector("#left-panel");
 const ok = await fetch("./Untitled.txt");
 const data = await ok.text();
+let index = 0;
 JSON.parse(data).forEach((element) => {
   let { previewImage, title } = element;
-  sidePanel.innerHTML += generateImageHtml(previewImage, title);
+  sidePanel.innerHTML += generateImageHtml(previewImage, title, index);
+  contractTitle(document.querySelector(`#div-${index++}`));
 });
 
 // render first image at right panel
@@ -33,7 +51,7 @@ const curName = document.querySelector("#name-box");
 const setImage = (item) => {
   let src = item.firstElementChild.firstElementChild.getAttribute("src");
   curPicture.setAttribute("src", src);
-  curName.value = item.lastElementChild.textContent;
+  curName.value = titleMap.get(item.lastElementChild.id);
 };
 
 let curIndex = 0;
@@ -72,5 +90,6 @@ curName.addEventListener("keydown", (event) => {
 
 curName.onchange = (event) => {
   let element = images[curIndex].lastElementChild;
-  element.innerHTML = generateTitleHtml(event.target.value);
+  element.innerHTML = event.target.value;
+  contractTitle(element);
 };
